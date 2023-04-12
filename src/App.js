c90fe680b361b59b031d7bafa094f942c66003ae
darkimpow <darkimpowell@gmail.com>
2023-04-12 11:33:13 -0400

    creating inovices tab with check box feature and respective colors for light and dark mode

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-/*import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+/*
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar"*/
+import Calendar from "./scenes/calendar"
+import Invoices from "./scenes/invoices";
+*/
+
 
 
 function App() {
@@ -30,8 +33,8 @@ function App() {
                     <Routes>
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/team" element={<Team />} />
-                        {/*<Route path="/contacts" element={<Contacts />} />
-                        <Route path="/invoices" element={<Invoices />} />
+                        <Route path="/contacts" element={<Contacts />} />
+                        {/*<Route path="/invoices" element={<Invoices />} />
                         <Route path="/form" element={<Form />} />
                         <Route path="/bar" element={<Bar />} />
                         <Route path="/pie" element={<Pie />} />
