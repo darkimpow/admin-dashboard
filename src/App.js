ec0f5b54b338b787dae3d80d73578eaefe27839e
darkimpow <darkimpowell@gmail.com>
2023-04-12 11:46:45 -0400

    created barchart, geography and line chart js files with respective code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Sidebar from "./scenes/global/sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+
 /*
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
@@ -14,7 +16,6 @@ import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar"
-import Invoices from "./scenes/invoices";
 */
 
 
@@ -34,8 +35,8 @@ function App() {
                         <Route path="/" element={<Dashboard />} />
                         <Route path="/team" element={<Team />} />
                         <Route path="/contacts" element={<Contacts />} />
-                        {/*<Route path="/invoices" element={<Invoices />} />
-                        <Route path="/form" element={<Form />} />
+                        <Route path="/invoices" element={<Invoices />} />
+                      {/*  <Route path="/form" element={<Form />} />
                         <Route path="/bar" element={<Bar />} />
                         <Route path="/pie" element={<Pie />} />
                         <Route path="/line" element={<Line />} />
